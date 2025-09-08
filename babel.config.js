aa660d46a3f8521d0d13fda06740dfc1ed6678f5
panicathe <ekdekdgkrp20@gmail.com>
2025-09-08 11:38:01 +0900

    ♻️ refactor: transportService 및 babel 설정에서 @env 제거 후 expo-constants 기반으로 변경

--- babel.config.js
+++ babel.config.js
@@ -7,9 +7,20 @@ module.exports = function (api) {
     ],
     plugins: [
       [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blocklist: null,
+          allowlist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+      [
         "module-resolver",
         {
-          root: ["."],
+          root: ["."], // app 폴더를 절대 경로의 시작점으로 설정
           alias: {
             "@app": "./app",
             "@tabs": "./app/(tabs)",
