67df36dbd7b82c1803050519b28bfa7a3d8e9fdb
seyeon <y.ee0nn58@gmail.com>
2025-09-10 15:32:28 +0900

    ✨ feat: 공통 여백, 글자 사이즈 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 
 /** @type {import('tailwindcss').Config} */
 const { Colors, Gradients } = require("./constants/Colors.cjs");
+const { spacing } = require("./constants/Design.cjs");
 
 module.exports = {
   content: [
@@ -21,6 +22,19 @@ module.exports = {
       spacing: {
         pageX: "16px",
         66: "66px",
+        none: `${spacing.none}px`,
+        xxs: `${spacing.xxs}px`,
+        xs: `${spacing.xs}px`,
+        sm: `${spacing.sm}px`,
+        md: `${spacing.md}px`,
+        lg: `${spacing.lg}px`,
+        llg: `${spacing.llg}px`,
+        xl: `${spacing.xl}px`,
+        "2xl": `${spacing["2xl"]}px`,
+        "3xl": `${spacing["3xl"]}px`,
+        "4xl": `${spacing["4xl"]}px`,
+        "5xl": `${spacing["5xl"]}px`,
+        "6xl": `${spacing["6xl"]}px`,
       },
       fontFamily: {
         sf: ["SFPro"],
@@ -31,6 +45,23 @@ module.exports = {
         "grotesk-md": ["SpaceGrotesk-Medium"],
         "grotesk-b": ["SpaceGrotesk-Bold"],
         "grotesk-l": ["SpaceGrotesk-Light"],
+      },
+      fontSize: {
+        h1: ["24", { lineHeight: "30px" }],
+        h2: ["20", { lineHeight: "26px" }],
+        h3: ["18", { lineHeight: "24px" }],
+        h4: ["16", { lineHeight: "22px" }],
+
+        bodyLg: ["18", { lineHeight: "26px" }],
+        body: ["16", { lineHeight: "22px" }],
+        bodySm: ["14", { lineHeight: "20px" }],
+
+        label: ["14", { lineHeight: "20px" }],
+        button: ["16", { lineHeight: "22px" }],
+
+        caption: ["12", { lineHeight: "16px" }],
+        footnote: ["11", { lineHeight: "15px" }],
+        overline: ["10", { lineHeight: "14px" }],
       },
     },
   },
